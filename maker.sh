84f694ca79452dcbb63bb1a584e8e135a5bcaf67
megahomyak <g.megahomyak@gmail.com>
2022-11-01 20:04:47 +0500

    Removed the `maker` shell script

--- maker.sh
+++
@@ -1,9 +0,0 @@
-export ROOT=$(pwd)
-for d in $(find . -type d -not -path '*/.*')
-do
-    if [ -f "${d}/Makefile" ]; then
-        make -f "Makefile" -s -C "${d}"
-    else
-        make -f "${ROOT}/Makerfile" -s -C "${d}"
-    fi
-done
