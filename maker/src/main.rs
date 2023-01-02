c17a8bb3f1d895436b0e88372e8bde4676c8330e
megahomyak <g.megahomyak@gmail.com>
2023-01-02 16:07:09 +0500

    Decided to take a different approach
    I will write the articles first and then parse them.

--- maker/src/main.rs
+++ maker/src/main.rs
@@ -1,13 +1,1 @@
-mod context;
-mod elements;
-mod html;
-mod special_types;
-mod utils;
-
-use walkdir::WalkDir;
-
-fn main() {
-    for entry in WalkDir::new(".").into_iter().map(Result::unwrap) {
-        let root = xmltree::Element::parse(std::fs::File::open(entry.path()).unwrap()).unwrap();
-    }
-}
+fn main() {}
